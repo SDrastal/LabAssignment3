bb7af0a039b3c49286cc20eae9f8477736bee39b
SDrastal <sa122032@ucf.edu>
2025-01-23 11:17:36 -0500

    Scripts

---
+++ Task 1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //Name of the class that will be calculated
+    public string className = "Science";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"The name of the class is " + className);
+    }
+
+    void challengeCalculator ()
+    {
+        int challengeRating;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
