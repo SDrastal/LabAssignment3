73edb17ea3a9c789644763c7f7b926dd90af7fc4
Joaquin Royer <joaqstarr@gmail.com>
2025-01-23 11:31:26 -0500

    Task 1 Complete

--- Task 1.cs
+++ Task 1.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     //Name of the class that will be calculated
-    public string className = "Science";
+    [SerializeField]
+    private string className = "Science";
 
+    [SerializeField, Range(0, 10)]
+    private int numberOfModules = 0;
+
+    [SerializeField, Range(0, 10)]
+    private int numberOfReadingMaterial = 0;
+
+    [SerializeField, Range(0, 10)]
+    private int numberOfQuizes = 0;
+
+    [SerializeField, Range(0, 10)]
+    private int numberOfAssignments = 0;
+
+    [SerializeField]
+    private bool hasInstructorTaughtCourse = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"The name of the class is " + className);
+        Debug.Log($"The name of the class is {className}.");
+
+        float difficulty = 0;
+        //add percentage scaled by number for each category
+        difficulty += ChallengeCalculator(15, numberOfModules);
+        difficulty += ChallengeCalculator(30, numberOfReadingMaterial);
+        difficulty += ChallengeCalculator(15, numberOfQuizes);
+        difficulty += ChallengeCalculator(30, numberOfAssignments);
+
+        //if instructor has not taught class, give 10 % difficulty
+        difficulty += ChallengeCalculator(10, (hasInstructorTaughtCourse) ? 0 : 10);
+
+        Debug.Log($"Its total difficulty is {difficulty}%");
     }
 
-    void challengeCalculator ()
+    //multiply total percentage by scalar from 0-10.
+    float ChallengeCalculator (float percentage, int difficulty)
     {
-        int challengeRating;
-
+        return percentage * (difficulty/10f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
