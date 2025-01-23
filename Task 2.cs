816e7d2a3b795fdb41a6cdc02e9962b388054998
SDrastal <sa122032@ucf.edu>
2025-01-23 11:54:52 -0500

    Update Task 2.cs
    Completed Task 2

--- Task 2.cs
+++ Task 2.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Task2 : MonoBehaviour
 {
+    // The variables that need to be around for all of the code
+    public float storeDiscount = 0.4f;
+    public int bookCost = 20;
+    public int bookAmount = 15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(CalculateWholesale(bookCost, bookAmount));
     }
 
-    // Update is called once per frame
-    void Update()
+    float CalculateWholesale(float bookCost, int copyAmount)
     {
-        
+        //Gets the total cost for the amount of books purchased
+        float totalCost = (bookCost * copyAmount) * (1 - storeDiscount);
+
+        //Gets the total cost of shipping the quanity of books
+        float shippingCost = 3 + (.75f * (copyAmount - 1));
+
+        //Returns the total cost of the shipping and books
+        return totalCost + shippingCost;
     }
+
 }
